a7332322c01c5dd32bb1363306414c1accabd3c1
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-03-16 21:09:33 +0300

    logo created and navbar builded

---
+++ app/components/layout/Header.jsx
@@ -0,0 +1,45 @@
+import Logo from "../ui/Logo";
+import { FaUserAlt, FaShoppingCart, FaSearch } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <div className="h-[7.5rem] bg-secondary">
+      <div className="container mx-auto text-white flex justify-between items-center h-full">
+        {/* <Logo /> */}
+        <img src="assets/png/fooder logo.png" alt="" className="h-[5.5rem] translate-y-0"/>
+        <nav>
+          <ul className="flex gap-x-2">
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Home</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Menu</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">About</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Book Table</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex gap-x-4 items-center">
+          <a href="#">
+            <FaUserAlt />
+          </a>
+          <a href="#">
+            <FaShoppingCart />
+          </a>
+          <a href="#">
+            <FaSearch />
+          </a>
+          <a href="#">
+            <button className="btn-primary">Order Online</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ app/components/ui/Logo.jsx
@@ -0,0 +1,6 @@
+
+const Logo = () => {
+  return <div className="text-[2rem] font-dancing font-bold">FoodER</div>;
+};
+
+export default Logo;
