d4d02d256de36a6dcae23b33c46f5c8f3e7e4645
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-03-18 19:07:04 +0300

    input component added

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import Campaigns from '../components/ui/Campaigns'
-import Carousel from "../components/ui/Carousel"
-import MenuWrapper from "../components/product/MenuWrapper"
-import About from "../components/About"
+import React from "react";
+import Campaigns from "../components/ui/Campaigns";
+import Carousel from "../components/ui/Carousel";
+import MenuWrapper from "../components/menu/MenuWrapper";
+import About from "../components/About";
+import Input from "../components/form/Input";
 
 const page = () => {
   return (
-    <div className='font-dancing text-primary' >
+    <div className="font-dancing text-primary">
       <Carousel />
       <Campaigns />
       <MenuWrapper />
-      <About/>
+      <About />
+      <div className="p-20 container mx-auto">
+        <Input />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
