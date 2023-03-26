39a0dd3df5bef93e3d0b516a4177cfb1d6f1c442
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-03-26 21:06:43 +0300

    user data sent to api from postman

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,8 +1,25 @@
+import User from "../../../models/User";
 import dbConnect from "../../../utils/dbConnect";
 
 const handler = async (req, res) => {
   await dbConnect();
-  res.status(200).json({ message: "You did it man!" });
-  req
-};
+  const { method } = req;
+  
+  if (method === "GET") {
+    try {
+      const users = await User.find({});
+      res.status(200).json({ success: true, data: users })
+   } catch (error) {
+      console.log(error);
+    }
+  }
+    if (method === "POST") {
+      try {
+        const newUser = await User.create(req.body);
+          res.status(200).json({ success: true, data: newUser });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+}
 export default handler;
