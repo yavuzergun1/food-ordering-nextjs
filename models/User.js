0b0d96c28e908cf3da84ec9ff6f6074e8b426d34
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-03-27 21:30:12 +0300

    register by form is done!

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema(
   {
-    name: {
+    fullName: {
       type: String,
       required: true,
     },
--- pages/api/users/register.js
+++ pages/api/users/register.js
@@ -1,5 +1,6 @@
 import User from "../../../models/User";
 import dbConnect from "../../../utils/dbConnect";
+import bcrypt from "bcrypt";
 
 const handler = async (req, res) => {
   await dbConnect();
@@ -8,12 +9,18 @@ const handler = async (req, res) => {
   if (user) {
     res.status(400).json({ error: "User already exists" });
     return;
-    }
-    
-    try {
-        const newUser = await new User(body);
-        const salt = await bcrypt.genSalt(10);
-    }
+  }
 
+  try {
+    const newUser = await new User(inputs);
+    //   password encryption
+    const salt = await bcrypt.genSalt(10);
+    newUser.password = await bcrypt.hash(newUser.password, salt);
+    newUser.confirmPassword = await bcrypt.hash(newUser.confirmPassword, salt);
+    await newUser.save();
+    res.status(200).json(newUser);
+  } catch (error) {
+    console.log(error);
+  }
 };
 export default handler;
