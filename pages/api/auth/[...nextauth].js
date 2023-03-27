ac30ab9c0252e1c9560f3fab272afe39d3c90846
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-03-27 15:37:47 +0300

    github user data successfully sended to mongodb

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,17 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../utils/mongo";
 
 export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    ],
-    pages: {
-        signIn:"/auth/login"
-    }
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
 });
