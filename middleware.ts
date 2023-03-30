b81efb5501368772dafc16a25359bce82b5fff82
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-03-30 22:47:15 +0300

    admin page protected by middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
-import { redirect } from "next/navigation";
 
 export function middleware(req: NextRequest) {
-  console.log("isAuth", req.cookies.has("token"));
   const isAuth = req.cookies.has("token");
-    if (isAuth) {
-      console.log("REQ",req.url);
-      
+  console.log(isAuth);
+
+  const url = req.url;
+  console.log("url", url);
+
+  if (isAuth && req.nextUrl.pathname === "/admin") {
     return NextResponse.redirect(
       new URL("/admin/profile", "http://localhost:3000/admin")
     );
   }
+      if(!isAuth && url.includes("/admin/profile")) {
+      const pathname = req.nextUrl.pathname;
+      return NextResponse.redirect("http://localhost:3000/admin");
+    }
 }
 
-export const config = {
-  matcher: "/admin",
-};
+// export const config = {
+//   matcher: "/admin",
+// };
