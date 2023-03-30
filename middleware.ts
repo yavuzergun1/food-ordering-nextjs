fa2de5e6813882048fa092aca523da8bec514043
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-03-30 23:10:42 +0300

    middleware host problem solved

--- middleware.ts
+++ middleware.ts
@@ -4,18 +4,23 @@ export function middleware(req: NextRequest) {
   const isAuth = req.cookies.has("token");
   console.log(isAuth);
 
+  const host = req.nextUrl.protocol + req.headers.get("host");
+  console.log("host", host);
+
   const url = req.url;
   console.log("url", url);
 
+  // if is logged in, it redirects to admin/profile page when you go to admin page
   if (isAuth && req.nextUrl.pathname === "/admin") {
     return NextResponse.redirect(
       new URL("/admin/profile", "http://localhost:3000/admin")
     );
   }
-      if(!isAuth && url.includes("/admin/profile")) {
-      const pathname = req.nextUrl.pathname;
-      return NextResponse.redirect("http://localhost:3000/admin");
-    }
+  // if you're not logged in, it redirects to /admin page when you go to admin/profile page
+  if (!isAuth && url.includes("/admin/profile")) {
+    const pathname = req.nextUrl.pathname;
+    return NextResponse.redirect(`${host}/admin`);
+  }
 }
 
 // export const config = {
