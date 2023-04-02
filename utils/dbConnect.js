e163be8e0d2b6ffa2fb6693e396174928fb5f92c
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-04-02 22:33:58 +0300

    buffer command false

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -26,7 +26,7 @@ async function dbConnect() {
 
   if (!cached.promise) {
     const opts = {
-      bufferCommands: false,
+      // bufferCommands: false,
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
