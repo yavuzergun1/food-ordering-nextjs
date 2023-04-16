d0f7d15374476a9bb6156e1c8d08e469fe0a17b1
Yavuz Ergun <95319292+yavuzergun1@users.noreply.github.com>
2023-04-16 15:38:58 +0300

    middleware added to route directory

---
+++ middleware.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  console.log("PATHNAME", req.nextUrl.pathname);
+  const isAdminAuth = req.cookies.has("admin_token");
+  // console.log(isAdminAuth);
+
+
+  const sessionToken = req.cookies.has("__Secure-next-auth.session-token");
+  // console.log("ISSESSION", sessionToken);
+
+  const host = req.nextUrl.protocol + req.headers.get("host");
+  // console.log("host", host);
+
+  const url = req.url;
+  // console.log("url", url);
+
+  // if is admin logged in, it redirects to admin/profile page when you go to admin page
+  if (isAdminAuth && req.nextUrl.pathname === "/admin") {
+    return NextResponse.redirect(
+      new URL("/admin/adminprofile/products")
+    );
+  }
+  // if you're not admin logged in, it redirects to /admin page when you go to admin/profile page
+  if (!isAdminAuth && url.includes("/admin/adminprofile")) {
+    const pathname = req.nextUrl.pathname;
+    return NextResponse.redirect(new URL(`${host}/admin`));
+  }
+
+  // user login control
+
+  if (sessionToken && req.nextUrl.pathname === "/auth/login") {
+    return NextResponse.redirect(
+     new URL(`${host}/profile/account`)
+    );
+  } if (!sessionToken && req.nextUrl.pathname.includes("/profile")) {
+    return NextResponse.redirect(new URL(`${host}/auth/login`));
+  }
+}
+
+export const config = {
+  matcher: ["/admin", "/profile"], // Add "/profile" path here
+};
