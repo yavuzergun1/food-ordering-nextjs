59214d0809f079390685fe848d65cbae91312122
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-04-16 10:49:02 +0300

    fonts and cdn canceled

--- app/head.jsx
+++ app/head.jsx
@@ -13,15 +13,15 @@ export default function Head() {
       <link
         href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&family=Josefin+Sans:wght@400;500;600&family=Oswald:wght@400;500;600&display=swap"
         rel="stylesheet"
-      /> */}
-      
+      />
+
       <link
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
         rel="stylesheet"
         integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
         crossOrigin="anonymous"
         referrerPolicy="no-referrer"
-      />
+      /> */}
     </>
   );
 }
