f0a0c13a8640b9e0674479baf01a9d6eb911c957
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-04-17 21:21:47 +0300

    profile image load to mongodb is done

--- app/profile/account/page.jsx
+++ app/profile/account/page.jsx
@@ -10,9 +10,13 @@ import axios from "axios";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import { getUser } from "../UserProfile";
+import { useRouter } from "next/navigation";
 
 const Account = () => {
+  const [file, setFile] = useState();
+  const [imageSrc, setImageSrc] = useState();
   const session = useSession();
+  const router = useRouter();
   // console.log("SESSION", session);
   const [user, setUser] = useState();
   const userId = session.data?.id;
@@ -35,6 +39,65 @@ const Account = () => {
 
   // console.log("USER", user);
 
+  // const onSubmit = async (values, actions) => {
+  //   try {
+  //     const res = await axios.put(
+  //       `${process.env.NEXT_PUBLIC_API_URL}/users/${user._id}`,
+  //       values
+  //     );
+
+  //     router.push("/profile/account");
+  //     if (res.status === 200) {
+  //       // toast.success("Profile updated successfully");
+  //       setUser(res?.data);
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+   const onSubmit = async () => {
+     const data = new FormData();
+     data.append("file", file);
+     data.append("upload_preset", "fooder");
+
+     try {
+       const uploadRes = await axios.post(
+         "https://api.cloudinary.com/v1_1/dz2y5zsex/image/upload",
+         data
+       );
+       // console.log(uploadRes);
+
+       const { url } = uploadRes.data;
+       const newProduct = {
+         img: url,
+         values,
+       };
+
+       const res = await axios.put(
+         `${process.env.NEXT_PUBLIC_API_URL}/users/${user._id}`,
+         newProduct
+       );
+
+       router.push("/profile/account");
+       if (res.status === 200) {
+         // toast.success("Profile updated successfully");
+         setUser(res?.data);
+       }
+
+       // console.log("added product", res.data);
+     } catch (err) {
+       console.log(err);
+       if (
+         err.response.data.error.message === "Unsupported source URL: undefined"
+       ) {
+         alert("Please upload an image");
+       } else {
+         alert("Something went wrong", err);
+       }
+     }
+   };
+
   const { values, errors, touched, handleSubmit, handleChange, handleBlur } =
     useFormik({
       enableReinitialize: true,
@@ -46,22 +109,21 @@ const Account = () => {
         job: user?.job || "",
       },
       validationSchema: profileSchema,
-      onSubmit: async (values, actions) => {
-        try {
-          const res = await axios.put(
-            `${process.env.NEXT_PUBLIC_API_URL}/users/${user._id}`,
-            values
-            );
-            console.log(values);
-          if (res.status === 200) {
-            // toast.success("Profile updated successfully");
-            console.log(res);
-          }
-        } catch (err) {
-          console.log(err);
-        }
-      },
+      onSubmit,
     });
+
+  // get photo from file input and set it to imageSrc
+  const handleOnChange = (changeEvent) => {
+    const reader = new FileReader();
+    reader.onload = function (onLoadEvent) {
+      setImageSrc(onLoadEvent.target.result);
+      setFile(changeEvent.target.files[0]);
+    };
+    reader.readAsDataURL(changeEvent.target.files[0]);
+    // console.log(imageSrc);
+  };
+
+ 
 
   const inputs = [
     {
@@ -118,6 +180,24 @@ const Account = () => {
         onSubmit={handleSubmit}
       >
         <Title addClass="text-[40px]">Account Settings</Title>
+        <div className="flex flex-col text-sm mt-6">
+          <label className="flex gap-2 items-center">
+            <input
+              type="file"
+              onChange={(e) => handleOnChange(e)}
+              className="hidden"
+            />
+            <button className="btn-primary cursor-pointer !rounded-none !bg-blue-600 pointer-events-none">
+              Choose an Image
+            </button>
+            {imageSrc && (
+              <div>
+                {/*eslint-disable-next-line @next/next/no-img-element*/}
+                <img src={imageSrc} alt="" className="w-12 h-12 rounded-full" />
+              </div>
+            )}
+          </label>
+        </div>
         <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-4">
           {inputs.map((input) => (
             <Input
@@ -132,7 +212,7 @@ const Account = () => {
           Update
         </button>
       </form>
-      {/* <ShowAccount user={user} /> */}
+      <ShowAccount user={user} />
     </div>
   );
 };
