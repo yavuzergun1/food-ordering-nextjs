154cbbd09f0b186fe6c106e52f943bc4b23f6317
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-04-19 17:46:57 +0300

    alert bug fixed. toastify added

--- app/Providers.jsx
+++ app/Providers.jsx
@@ -3,11 +3,14 @@
 import store from "../redux/store";
 import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
+import { ToastContainer } from "react-toastify";
 
 const Providers = ({ children }) => {
   return (
     <SessionProvider>
-      <Provider store={store}>{children}</Provider>
+      <Provider store={store}>
+        <ToastContainer/>
+        {children}</Provider>
     </SessionProvider>
   );
 };
