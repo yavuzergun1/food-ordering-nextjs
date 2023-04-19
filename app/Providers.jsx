24fb26ea1e50b9705efc3e910219f92539f1138d
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-04-19 21:51:58 +0300

    Revert "alert bug fixed. toastify added"
    This reverts commit 154cbbd09f0b186fe6c106e52f943bc4b23f6317.

--- app/Providers.jsx
+++ app/Providers.jsx
@@ -3,14 +3,11 @@
 import store from "../redux/store";
 import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
-import { ToastContainer } from "react-toastify";
 
 const Providers = ({ children }) => {
   return (
     <SessionProvider>
-      <Provider store={store}>
-        <ToastContainer/>
-        {children}</Provider>
+      <Provider store={store}>{children}</Provider>
     </SessionProvider>
   );
 };
