9f805b0b1cc0711d49e514897d2b02793295f055
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-04-27 20:43:24 +0300

    admin categories page tsx convert is done

--- app/admin/adminprofile/categories/page.tsx
+++ app/admin/adminprofile/categories/page.tsx
@@ -6,45 +6,59 @@ import Title from "../../../../components/ui/Title";
 import axios from "axios";
 import useSWR from "swr";
 
-const Category = () => {
-  const [inputText, setInputText] = useState("");
-  const [categories, setCategories] = useState([]);
+type Category = {
+  _id: string;
+  title: string;
+  data: {
+    title: string;
+    _id: string;
+  }
+};
 
-  const fetcher = async () =>
+const Category = (): JSX.Element => {
+  const [inputText, setInputText] = useState<string>("");
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const fetcher = async (): Promise<Category[]> =>
     await axios
-      .get(`${process.env.NEXT_PUBLIC_API_URL}/categories`)
+      .get<Category[]>(`${process.env.NEXT_PUBLIC_API_URL}/categories`)
       .then((res) => res.data);
-  const { data, error, isLoading } = useSWR(
+  const { data, error, isLoading } = useSWR<Category[]>(
     `${process.env.NEXT_PUBLIC_API_URL}/categories`,
     fetcher
   );
-  if (error) return console.log(error);
-  if (isLoading) return (
-    <div className="flex w-full items-center m-auto justify-center h-screen">
-      <div className="animate-spin w-8 h-8 border-t-4 border-blue-500 border-solid rounded-full"></div>
-    </div>
-  );
+  if (error) {
+    // @ts-ignore
+    return console.log(error);
+  }
+  if (isLoading)
+    return (
+      <div className="flex w-full items-center m-auto justify-center h-screen">
+        <div className="animate-spin w-8 h-8 border-t-4 border-blue-500 border-solid rounded-full"></div>
+      </div>
+    );
 
-  const addCategory = async (e) => {
+  const addCategory = async (e: React.MouseEvent<HTMLButtonElement>) => {
     try {
       const res = await axios.post(
         `${process.env.NEXT_PUBLIC_API_URL}/categories`,
         { title: inputText }
       );
+      // @ts-ignore
       setCategories([...data, res.data]);
-      setInputText("");
     } catch (err) {
       console.log(err);
     }
   };
 
-  const deleteCategory = async (e, id) => {
+  const deleteCategory = async (e: React.MouseEvent<HTMLButtonElement>, id: string) => {
     e.preventDefault();
     try {
       if (confirm("Are you sure you want to delete this category?")) {
         await axios.delete(
           `${process.env.NEXT_PUBLIC_API_URL}/categories/${id}`
         );
+        // @ts-ignore
         setCategories(data.filter((cat) => cat._id !== id));
       }
     } catch (err) {
@@ -58,7 +72,9 @@ const Category = () => {
         <div className="flex gap-4 flex-1 items-center">
           <Input
             placeholder="Add a new Category..."
-            onChange={(e) => setInputText(e.target.value)}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInputText(e.target.value)
+            }
             value={inputText}
           />
           <button className="btn-primary" onClick={addCategory}>
@@ -67,6 +83,7 @@ const Category = () => {
         </div>
         <div className="mt-10 max-h-[250px] overflow-auto pb-4">
           {/* if first opened page, use data, if any change like add or delete, use categories */}
+          {/*    @ts-ignore */}
           {(categories.length > 0 ? categories : data).map(
             (category, index) => (
               <div className="flex justify-between mt-4" key={index}>
