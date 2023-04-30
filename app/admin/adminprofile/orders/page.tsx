ad8108f216f35063c3264732e876e29e08642082
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-04-30 19:57:52 +0300

    adminprofile/orders fetching build bug is fixed

--- app/admin/adminprofile/orders/page.tsx
+++ app/admin/adminprofile/orders/page.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Orders from "./Orders";
 
-async function page () {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/orders`, { cache: "no-cache" });
-  const orders= await res.json()
+async function page() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/orders`, {
+    cache: "no-store",
+  });
+  const orders = await res.json();
   console.log("orders", orders);
 
   return (
     <div>
-      {
-    orders && <Orders orders={orders} />
-      }
-    </div> 
+      <Orders orders={orders} />
+    </div>
   );
-};
+}
 
 export default page;
