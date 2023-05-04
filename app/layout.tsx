24d9d24233f7c683857eaefaabe57be3dcf695a5
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-05-04 13:51:20 +0300

    layout overflow-x-hidden

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>
+      <body className="overflow-x-hidden">
         <Providers>
           <Header />
           {children}
