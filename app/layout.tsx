27e269a155ae51fb6a291a0c4841be3ed12ea7b1
yavuzergun1 <posta.yavuzergun@gmail.com>
2023-05-04 14:37:56 +0300

    overflow bug fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className="overflow-x-hidden">
+      <body>
         <Providers>
           <Header />
           {children}
